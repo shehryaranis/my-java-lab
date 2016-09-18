01724574a4f29765fb88def99c6fcff0e42743dd
shehryar <bhaijan42301@gmail.com>
2016-09-18 04:27:21 -0700

    my java work

---
+++ app.js
@@ -0,0 +1,27 @@
+ var x = prompt("your percentage")
+ if( x >= 90 && x < 100 ){
+alert("A+")
+ }
+ else if( x >= 80 && x < 90) {
+     alert("A")
+
+ }
+  else if( x >= 70 && x < 80) {
+     alert("B")
+
+ }
+  else if( x >= 60 && x < 70) {
+     alert("C")
+
+ }
+  else if( x >= 50 && x < 60) {
+     alert("E")
+
+ }
+  else if( x >= 40 && x < 50) {
+     alert("F")
+
+ }
+ else{
+     alert("FAIL" )
+ }
